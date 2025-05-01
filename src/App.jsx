225873158aff685d2b899c4e736daab2697ee47e
BloodyRoom <158325416+BloodyRoom@users.noreply.github.com>
2025-05-01 08:12:21 +0000

    feat: basic logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [password, setPassword] = useState("");
+
+  const generate = (length = 8) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    console.log(result);
+    setPassword(result);
+  }
 
   return (
     <>
-      
+      <h2>Password Generator</h2>
+      <div style={{display: "flex", gap: "10px"}}>
+        <input type="text" value={password} readOnly/>
+        <button onClick={() => {generate()}}>Generate</button>
+      </div>
     </>
   )
 }
